b9737b21fe870fc23ffb85cd599c27cf56da7e43
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-05-24 15:11:12 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+# Infinity Bots
+
+import os
+import pyrogram
+from pyrogram import Client
+
+from config import Config  
+
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    JEBotZ = pyrogram.Client(
+        "CaptionBot",
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins,
+        workers=300
+    )
+    JEBotZ.run()
