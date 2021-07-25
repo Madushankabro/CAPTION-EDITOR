4f38341e8eff6d6d37025dcff713f56558ee54eb
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-07-25 23:48:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,4 +4,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-  await message.reply("Hey there, I'm channel caption bot. \n\nSource code => https://github.com/ImJanindu/channel-caption-bot \n\n**@JEBotZ**")
+  await message.reply("Hey there, I'm 𝑪𝑨𝑷𝑻𝑰𝑶𝑵 𝑬𝑫𝑰𝑻𝑶𝑹 𝑩𝑶𝑻.\nI can add a caption for every document file sent to a channel. \n\nPowered by => https://t.me/epusthakalaya_bots \n\n**@EPUSTHAKALAYABOTz**")
